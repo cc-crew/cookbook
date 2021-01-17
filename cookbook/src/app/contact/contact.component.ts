740d2a64e8823f16b4c5abcb979d5b884ddbd307
haeul <lea.haeussermann@akka.eu>
2021-01-17 23:52:21 +0100

    recipe-info

--- cookbook/src/app/contact/contact.component.ts
+++ cookbook/src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeInfoDialogComponent} from '../recipe-info-dialog/recipe-info-dialog.component';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
+  showRecipeInfo() {
+    this.modalService.open(RecipeInfoDialogComponent);
+  }
 }
