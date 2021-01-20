7ebcb7942ed0b1c0d10eaa7c2bf7c4007de31610
haeul <lea.haeussermann@akka.eu>
2021-01-20 16:42:19 +0100

    rezept

--- cookbook/src/app/data/baking-recipes.ts
+++ cookbook/src/app/data/baking-recipes.ts
@@ -1,32 +1,41 @@
 export default [
   {
     recipe: {
-      label: 'Flower Cupcakes',
-      image: 'https://www.edamam.com/web-img/8af/8af000c5cce9ca42825290938904b4fa.jpg',
-      creator: 'Giesela Bauer',
-      portions: '2-3 Portionen',
+      label: 'Käsekuchen',
+      image: './assets/images/kaesekuchen.jpg',
+      creator: '',
+      portions: '28cm Backform rund',
       ingredientLines: [
-        {amount: '18', unit: 'tablespoons (2 1/4 sticks)', ingredient: 'unsalted butter at room temperature' },
-        {amount: '2 2/3', unit: 'cups', ingredient: 'sugar' },
-        {amount: '6', unit: 'pcs', ingredient: 'eggs at room temperature' },
-        {amount: '1', unit: 'cup', ingredient: 'sour cream at room temperature' },
-        {amount: '1 1/2', unit: 'teaspoons', ingredient: 'pure vanilla extract' },
-        {amount: '3', unit: 'cups', ingredient: 'flour' },
-        {amount: '1/3', unit: 'cups', ingredient: 'cornstarch' },
-        {amount: '1', unit: 'teaspoon', ingredient: 'kosher salt' },
-        {amount: '1', unit: 'teaspoon', ingredient: 'baking soda' }
+        {amount: '250', unit: 'g', ingredient: 'Mehl (Mürbteig)' },
+        {amount: '125', unit: 'g', ingredient: 'Butter/ Margarine (Raumtemperatur) (Mürbteig)' },
+        {amount: '65', unit: 'g', ingredient: 'Zucker (Mürbteig)' },
+        {amount: '1', unit: 'St', ingredient: 'Ei (Raumtemperatur) (Mürbteig)' },
+        {amount: '1', unit: 'Prise', ingredient: 'Salz (Mürbteig)' },
+        {amount: '100', unit: 'g', ingredient: 'weiche Butter/ Margarine (Quarkmasse)' },
+        {amount: '200', unit: 'g', ingredient: 'Zucker (Quarkmasse)' },
+        {amount: '1', unit: 'Päckchen', ingredient: 'Vanillezucker (Quarkmasse)' },
+        {amount: '5', unit: 'St', ingredient: 'große Eier (Quarkmasse)' },
+        {amount: '750', unit: 'g', ingredient: 'Magerquark (Quarkmasse)' },
+        {amount: '3', unit: 'EL', ingredient: 'saure Sahne (Quarkmasse)' },
+        {amount: '1/4', unit: 'l', ingredient: 'Milch (Quarkmasse)' },
+        {amount: '80', unit: 'g', ingredient: 'Mondamin (Quarkmasse)' },
+        {amount: '1', unit: 'St', ingredient: 'Zitrone, der Saft (Quarkmasse)' }
       ],
       instructions: [
-        'Mehl und Zucker vermischen llllllllll hiuiuzhiuhiu iubiubiubiubiubiu jhuvu iuiubiubiub',
-        'Backofen vorheizen',
-        'Milch hinzugeben',
-        'Dann was andered',
-        'Was nächstes machen',
-        'In den Backofen schieben',
-        'Toppings zubereiten',
-        'Nach 60 min aud Backofen holen',
-        'Bestreuen',
-        'Am besten warm essen'
-      ]    }
+        'Backform Ø 28cm, Backofen auf 160°C vorheizen (Ober-/ Unterhitze), 55 min Backzeit',
+        'Zuerst den Mürbteig aus den oben genannten Zutaten herstellen (durch Hacken oder mit dem Rührgerät).',
+        'Mürbteig 30min kalt stellen.',
+        'Für die Quarkmasse die weiche Butter/ Margarine mit dem Zucker schaumig rühren.',
+        'Eier trennen. Die Eigelb langsam dazu einrühren.',
+        'Saft der Zitrone ebenfalls einrühren.',
+        'Den Quark, die saure Sahne, die Milch und das Mondamin dazugeben und alles langsam zu einer homogenen Masse verrühren.',
+        'Zuletzt den Eischnee steif schlagen und vorsichtig unterrühren.',
+        'Nun die Backform mit dem Mürbteig auslegen, dabei einen Rand von 2cm lassen.',
+        'Die Quarkmasse darauf geben.',
+        'Nach Belieben können Mandarinen oder Sauerkirschen dazu gegeben werden.',
+        'Nun den Kuchen in der vorgeheizten Ofen auf der mittleren Schiene schieben.',
+        'Nach 55 min den Kuchen herausholen (die Oberfläche sollte goldgelb sein) und für mehrere Stunden in der Backform abkühlen lassen.',
+      ]
+    }
   }
 ];
